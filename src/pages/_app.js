961707ef268c86712c57aeb5b13397885d3d8830
Alex Lee <alex@uniteus.com>
2020-03-25 22:02:17 -0400

    Initial commit with bootstrap and next js setup

---
+++ src/pages/_app.js
@@ -0,0 +1,23 @@
+import App from 'next/app'
+import Head from 'next/head';
+import './App.scss';
+
+
+class MyApp extends App {
+  render() {
+    const { Component } = this.props;
+
+    return (
+      <>
+        <Head>
+          <title>Thoughts!</title>
+        </Head>
+        <div className="body">
+          <Component {...this.props} />
+        </div>
+      </>
+    );
+  }
+}
+
+export default MyApp;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import HeaderNav from '../components/HeaderNav';
+import { Button } from 'reactstrap';
+
+const Index = () => {
+  return (
+    <div>
+      <HeaderNav />
+      <Button color="primary">primary</Button>{' '}
+      Hello Next.js
+    </div>
+  )
+}
+
+export default Index;
