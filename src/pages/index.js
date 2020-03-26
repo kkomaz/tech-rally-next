40f194b9d6f2f9967d5b21c484b74e0eb756f4c2
Alex Lee <alex@uniteus.com>
2020-03-25 22:25:29 -0400

    Button styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,10 @@ const Index = () => {
     <div>
       <HeaderNav />
       <Button color="primary">primary</Button>{' '}
+      <Button color="info">info</Button>{' '}
+      <Button color="secondary">secondary</Button>{' '}
+      <Button color="link">link</Button>
+
       Hello Next.js
     </div>
   )
