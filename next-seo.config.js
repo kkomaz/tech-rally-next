540ac1c4d529c634a8eb45acd868f09c2348c35b
Alex Lee <alex@uniteus.com>
2020-05-04 23:39:04 -0400

    Seo configs

---
+++ next-seo.config.js
@@ -0,0 +1,23 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en-IE',
+    url: 'https://techrally.me',
+    site_name: 'techrally',
+    description: 'Helping current and aspiring Software Developers with anything related to coding, design, and practical skills',
+    images: [
+      {
+        url: 'https://tech-rally-test-bucket.s3.us-east-2.amazonaws.com/branding.png',
+        width: 800,
+        height: 600,
+        alt: 'tech-rally-branding',
+      },
+    ],
+    title: 'techrally | One stop shop to learning the newest web technologies'
+  },
+  twitter: {
+    handle: '@TheTechRally',
+    site: '@TheTechRally',
+    cardType: 'summary_large_image',
+  }
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,28 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import Socials from 'components/Socials';
+import { DefaultSeo } from 'next-seo';
 import './_app.scss';
 import 'react-quill/dist/quill.snow.css';
+import SEO from '../../next-seo.config';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="main-container">
-      <div className="body">
-        <Component {...pageProps} />
+    <>
+      <DefaultSeo {...SEO} />
+      <div className="main-container">
+        <div className="body">
+          <Component {...pageProps} />
+        </div>
+        <div className="footer">
+          <Socials color="#FFFAFA" />
+          <h5
+            className="mt-one copy-right"
+          >
+            TechRally ©2020
+          </h5>
+        </div>
       </div>
-      <div className="footer">
-        <Socials color="#FFFAFA" />
-        <h5
-          className="mt-one copy-right"
-        >
-          TechRally ©2020
-        </h5>
-      </div>
-    </div>
+    </>
   );
 }
 
